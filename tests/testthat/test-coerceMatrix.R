10ceecc639e5af20a241cb86fa4f7d1429dc8ddf
Philippe Boileau <pboileau.swim@gmail.com>
2020-02-04 08:04:20 -0800

    coerceMatrix steps now skip on Travis CI

--- tests/testthat/test-coerceMatrix.R
+++ tests/testthat/test-coerceMatrix.R
@@ -1,8 +1,8 @@
 context("Test routine for coercing inputs to matrix objects")
 library(Matrix)
 
+skip_on_travis()
 test_that("Coercion of sparse matrices to standard matrices works", {
-  skip_on_travis("Bug in Travis CI")
   expect_equal(
     class(coerceMatrix(data = as(as.matrix(background_df), "dgCMatrix"))),
     "matrix"
