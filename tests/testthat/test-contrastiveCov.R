aa29fabaf6e2e95f62ec918637dcaafa9009eca4
Philippe Boileau <pboileau.swim@gmail.com>
2020-12-17 11:25:38 -0800

    updating contrastive cov and test

--- tests/testthat/test-contrastiveCov.R
+++ tests/testthat/test-contrastiveCov.R
@@ -10,28 +10,30 @@ dgC_toy <- as(as.matrix(toy_df[, -31]), "dgCMatrix")
 dm_back <- DelayedArray(background_df)
 dm_toy <- DelayedArray(toy_df[, -31])
 
-test_that("performs without issue when matrices are centered or scaled", {
-  
+test_that(paste("performs without issue when matrices are centered or scaled,",
+                "and when using ScaledMatrix objects"), {
+
   # normal dataframes
   expect_silent(contrastiveCov(toy_df[, -31], background_df, c(0, 1, 2),
-    center = TRUE, scale = TRUE
+    center = TRUE, scale = TRUE, scaled_matrix = TRUE
   ))
   expect_silent(contrastiveCov(toy_df[, -31], background_df, c(0, 1, 2),
     center = FALSE, scale = TRUE
   ))
   expect_silent(contrastiveCov(toy_df[, -31], background_df, c(0, 1, 2),
-    center = TRUE, scale = FALSE
+    center = TRUE, scale = FALSE, scaled_matrix = TRUE
   ))
   expect_silent(contrastiveCov(toy_df[, -31], background_df, c(0, 1, 2),
     center = FALSE, scale = FALSE
   ))
-  
+
   # sparse matrices
   expect_silent(contrastiveCov(dgC_toy, dgC_back, c(0, 1, 2),
                                center = TRUE, scale = TRUE
   ))
   expect_silent(contrastiveCov(dgC_toy, dgC_back, c(0, 1, 2),
-                               center = FALSE, scale = TRUE
+                               center = FALSE, scale = TRUE,
+                               scaled_matrix = TRUE
   ))
   expect_silent(contrastiveCov(dgC_toy, dgC_back, c(0, 1, 2),
                                center = TRUE, scale = FALSE
@@ -39,16 +41,18 @@ test_that("performs without issue when matrices are centered or scaled", {
   expect_silent(contrastiveCov(dgC_toy, dgC_back, c(0, 1, 2),
                                center = FALSE, scale = FALSE
   ))
-  
+
   # DelayedMatrices
   expect_silent(contrastiveCov(dm_toy, dm_back, c(0, 1, 2),
-                               center = TRUE, scale = TRUE
+                               center = TRUE, scale = TRUE,
+                               scaled_matrix = TRUE
   ))
   expect_silent(contrastiveCov(dm_toy, dm_back, c(0, 1, 2),
                                center = FALSE, scale = TRUE
   ))
   expect_silent(contrastiveCov(dm_toy, dm_back, c(0, 1, 2),
-                               center = TRUE, scale = FALSE
+                               center = TRUE, scale = FALSE,
+                               scaled_matrix = TRUE
   ))
   expect_silent(contrastiveCov(dm_toy, dm_back, c(0, 1, 2),
                                center = FALSE, scale = FALSE
@@ -65,23 +69,25 @@ test_that(paste(
   )), 1)
   expect_equal(length(contrastiveCov(toy_df[, -31],
     background_df, c(0, 1, 2),
-    center = TRUE, scale = TRUE
+    center = TRUE, scale = TRUE, scaled_matrix = TRUE
   )), 3)
-  
+
   # sparse matrices
   expect_equal(length(contrastiveCov(dgC_toy, dgC_back, 100,
-                                     center = TRUE, scale = TRUE
+                                     center = TRUE, scale = TRUE,
+                                     scaled_matrix = TRUE
   )), 1)
   expect_equal(length(contrastiveCov(dgC_toy, dgC_back, c(0, 1, 2),
                                      center = TRUE, scale = TRUE
   )), 3)
-  
+
   # DelayedMatrices
   expect_equal(length(contrastiveCov(dm_toy, dm_back, 100,
                                      center = TRUE, scale = TRUE
   )), 1)
   expect_equal(length(contrastiveCov(dm_toy, dm_back, c(0, 1, 2),
-                                     center = TRUE, scale = TRUE
+                                     center = TRUE, scale = TRUE,
+                                     scaled_matrix = TRUE
   )), 3)
 })
 
@@ -89,21 +95,22 @@ test_that(paste(
   "Number of columns of contrastive covariance matrix matches",
   "the number of columns in the target and background data"
 ), {
-  
+
   # data frames
   dim_cov <- ncol(contrastiveCov(toy_df[, -31], background_df, 4,
     center = TRUE, scale = TRUE
   )[[1]])
   expect_equal(dim_cov, ncol(toy_df[, -31]))
   expect_equal(dim_cov, ncol(background_df))
-  
+
   # sparse matrices
   dim_cov <- ncol(contrastiveCov(dgC_toy, dgC_back, 4,
-                                 center = TRUE, scale = TRUE
+                                 center = TRUE, scale = TRUE,
+                                 scaled_matrix = TRUE
   )[[1]])
   expect_equal(dim_cov, ncol(toy_df[, -31]))
   expect_equal(dim_cov, ncol(background_df))
-  
+
   # DelayedMatrices
   dim_cov <- ncol(contrastiveCov(dm_toy, dm_back, 4,
                                  center = TRUE, scale = TRUE
@@ -117,7 +124,7 @@ test_that(paste(
   "sequential analog"
 ), {
   register(SerialParam())
-  
+
   # regular data frame
   expect_equal(
     contrastiveCov(toy_df[, -31],
@@ -129,17 +136,17 @@ test_that(paste(
       center = TRUE, scale = TRUE
     )
   )
-  
+
   # sparse matrix
   expect_equal(
     contrastiveCov(dgC_toy, dgC_back, c(0, 1, 2),
-                   center = TRUE, scale = TRUE
+                   center = TRUE, scale = TRUE, scaled_matrix = TRUE
     ),
     bpContrastiveCov(dgC_toy, dgC_back, c(0, 1, 2),
-                     center = TRUE, scale = TRUE
+                     center = TRUE, scale = TRUE, scaled_matrix = TRUE
     )
   )
-  
+
   # DelayedMatrix
   expect_equal(
     contrastiveCov(dm_toy, dm_back, c(0, 1, 2),
